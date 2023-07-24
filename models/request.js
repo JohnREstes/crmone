bed3c33e63acd17f4fcb05cea5387b383d60cbff
ar161 <59390149+ar161@users.noreply.github.com>
2023-07-24 18:15:40 +0530

    Update request.js

--- models/request.js
+++ models/request.js
@@ -18,38 +18,3 @@ const requestSchema = new mongoose.Schema({
 const Request = mongoose.model('Request', requestSchema);
 
 module.exports = Request;
-
-// const mongoose = require('mongoose');
-
-// const requestSchema = new mongoose.Schema({
-//   type: {
-//     type: String,
-//     enum: ['ticket', 'issue', 'bug'],
-//     required: true,
-//   },
-//   name: {
-//     type: String,
-//     required: true,
-//   },
-//   tag: {
-//     type: String,
-//     enum: ['feature', 'fix', 'ui', 'danger', 'urgent'],
-//     required: true,
-//   },
-//   owner: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true,
-//   },
-//   deadline: {
-//     type: Date,
-//     required: true,
-//   },
-//   status: {
-//     type: String,
-//     enum: ['open', 'in progress', 'closed', 'resolved'],
-//     required: true,
-//   },
-// });
-
-// module.exports = mongoose.model('Request', requestSchema);
